5c42c2856cd0db4a333391225242d29c91de80ff
fdourmane <dourmanefarouk@gmail.com>
2023-01-30 22:23:32 +0400

    added content types to dashboard

--- src/api/tourism-service/controllers/tourism-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tourism-service controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::tourism-service.tourism-service');
--- src/api/tourism-service/routes/tourism-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tourism-service router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::tourism-service.tourism-service');
